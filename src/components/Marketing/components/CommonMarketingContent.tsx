41b387388c0916a98dab91fd4e9dda365a218694
abrightkite <phy5397@gmail.com>
2022-08-03 05:02:34 +0900

    feat(Marketing): 마케팅 content의 틀이되는 component 생성

---
+++ src/components/Marketing/components/CommonMarketingContent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CommonMarketingContent = () => {
+  return <div className="common-marketing-content">CommonMarketingContent</div>;
+};
+
+export default CommonMarketingContent;
